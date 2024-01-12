17ff2c28492fabfc4207d59e38d63497056b4cae
SmitMedkart <smit.b@medkart.in>
2024-01-12 18:50:40 +0530

    first-commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// const connectTomongo = require('./db');
+const express = require('express');
+let cors = require('cors');
+let app = express();
+const mongoose = require('mongoose');
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://0.0.0.0:27017/todos")
+// connectTomongo();
+
+app.use('/api/tasks', require('./routes/task'));
+
+
+app.listen(5000, ()=>{
+  console.log("Working");
+})
